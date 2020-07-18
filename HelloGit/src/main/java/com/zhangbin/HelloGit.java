6d563b3d40473109db071a2df5d0585a60837062
zb3352689 <59084746@qq.com>
2020-07-18 11:28:42 +0800

    创建分支3

--- HelloGit/src/main/java/com/zhangbin/HelloGit.java
+++ HelloGit/src/main/java/com/zhangbin/HelloGit.java
@@ -7,6 +7,7 @@ public class HelloGit {
         System.out.println("更新1");
         System.out.println("创建分支");
         System.out.println("创建分支2");
+        System.out.println("创建分支3");
     }
 
 }
